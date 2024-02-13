050c8417e7e78ddbe4585d2b43f95d5232b5a087
bean <titless.skittle@gmail.com>
2024-02-13 13:01:20 +0000

    add assets from mockup, switch to URP

---
+++ Assets/Scripts/Camera/CameraTarget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Camera
+{
+    [Serializable]
+    public class CameraTarget
+    {
+        public CameraPosition Position;
+        public Transform Target = null!;
+
+        public CameraSwitchOption[] SwitchOptions = Array.Empty<CameraSwitchOption>();
+    }
+}
