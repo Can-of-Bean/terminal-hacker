c6cfe8a24a5790d76c98e66f8b5bdb606fc61662
Solar <bmacaulay4843@gmail.com>
2024-02-22 12:12:57 +0000

    Added content to the files class

--- Assets/Scripts/Files/File.cs
+++ Assets/Scripts/Files/File.cs
@@ -3,10 +3,23 @@ namespace Files
     public class File : IFileSystemItem
     {
         public string Name { get; }
+        public string Content { get; set; }
 
         public File(string name)
         {
             Name = name;
+            Content = "";
+        }
+
+        public File(string name, string content)
+        {
+            Name = name;
+            Content = content;
+        }
+
+        public override string ToString()
+        {
+            return Name;
         }
     }
 }
