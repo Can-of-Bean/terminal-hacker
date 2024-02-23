fbe5ab7914aa1788be4fe3e99672e6b19c261937
Fae Burns <fae.burns0@gmail.com>
2024-02-23 17:51:51 +0000

    implemented user input

---
+++ Assets/Scripts/Terminal/LoopbackControlTarget.cs
@@ -0,0 +1,10 @@
+﻿namespace Terminal
+{
+    public class LoopbackControlTarget : TerminalControlTarget
+    {
+        public override void HandleUserInput(string message)
+        {
+            TerminalControl.Instance.WriteLineToConsole(TerminalControl.Instance.InputHeader + "\n" + message);
+        }
+    }
+}
---
+++ Assets/Scripts/Terminal/TerminalControlTarget.cs
@@ -0,0 +1,14 @@
+﻿namespace Terminal
+{
+    /// <summary>
+    /// A base class implemented by all normal receivers of terminal input.
+    /// </summary>
+    public abstract class TerminalControlTarget
+    {
+        /// <summary>
+        /// Handles text coming in from the user.
+        /// </summary>
+        /// <param name="message"></param>
+        public abstract void HandleUserInput(string message);
+    }
+}
