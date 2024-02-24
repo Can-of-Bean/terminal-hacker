4da141dd447e02101d0c4d757110113e0cafd055
Solar <bmacaulay4843@gmail.com>
2024-02-24 22:50:07 +0000

    Created CommandControl class
    This class is responsible for executing the commands and reporting errors

---
+++ Assets/Scripts/Commands/CommandControl.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using Exceptions;
+using Terminal;
+
+namespace Commands
+{
+    public class CommandControl : SingletonBehaviour<CommandControl>
+    {
+        private Dictionary<string, ICommand> commands;
+
+        public void Start()
+        {
+            TerminalControl.Instance.RawInputSubmitted += OnRawInputSubmitted;
+
+            // Initialize the commands dictionary
+            commands = new Dictionary<string, ICommand>
+            {
+                { "ls", new ListCommand() },
+                { "cat", new CatCommand() },
+                { "ssh", new SshCommand()},
+                { "cd", new ChangeDirectoryCommand() }
+                // { "mkdir", new MakeDirectoryCommand() },
+                // { "touch", new TouchCommand() },
+                // { "rm", new RemoveCommand() },
+                // { "clear", new ClearCommand() },
+                // { "pwd", new PrintWorkingDirectoryCommand() },
+                // { "echo", new EchoCommand() },
+                // { "help", new HelpCommand() }
+            };
+        }
+
+        private void OnRawInputSubmitted(object sender, TerminalInputEventArgs e)
+        {
+            string cmd = e.Input.Split(' ')[0];
+            string[] args = e.Input.Split(' ')[1..] ?? Array.Empty<string>();
+
+            if (commands.TryGetValue(cmd, out ICommand command))
+            {
+                try
+                {
+                    command.Execute(args);
+                }
+                catch (CommandException ex)
+                {
+                    TerminalControl.Instance.WriteLineToConsole($"Error: {ex.Message}");
+                }
+                catch (FileSystemException ex)
+                {
+                    TerminalControl.Instance.WriteLineToConsole($"Error: {ex.Message}");
+                }
+            }
+            else
+            {
+                TerminalControl.Instance.WriteLineToConsole($"Unknown command: {cmd}");
+            }
+        }
+    }
+}
