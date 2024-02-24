76ba3daad1678ee478f6eea94cf717b0eee965bb
Solar <bmacaulay4843@gmail.com>
2024-02-24 22:53:10 +0000

    Fixed get item for more complex paths and added GetPath method

--- Assets/Scripts/Files/Directory.cs
+++ Assets/Scripts/Files/Directory.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using Exceptions;
+using UnityEngine;
 
 namespace Files
 {
@@ -6,11 +8,19 @@ namespace Files
     {
         public string Name { get; }
         public List<IFileSystemItem> Items { get; }
+        public Directory? Parent { get; set; }
 
         public Directory(string name)
         {
             Name = name;
             Items = new List<IFileSystemItem>();
+        }
+
+        public Directory(string name, Directory parent)
+        {
+            Name = name;
+            Items = new List<IFileSystemItem>();
+            Parent = parent;
         }
 
         public T AddItem<T>(T item) where T : IFileSystemItem
@@ -25,9 +35,65 @@ namespace Files
             return item;
         }
 
-        public IFileSystemItem GetItem(string name)
+        public IFileSystemItem GetItem(string path)
         {
-            return Items.Find(item => item.Name == name);
+            string[] pathParts = path.Split('/');
+            IFileSystemItem current = this;
+            for (int i = 0; i < pathParts.Length; i++)
+            {
+                string part = pathParts[i];
+                if (part == "..")
+                {
+                    if (current is Directory directory)
+                    {
+                        current = directory.Parent ?? throw new FileSystemException("Cannot go up from root.");
+                    }
+                    else
+                    {
+                        throw new FileSystemException($"{current.Name} is not a directory.");
+                    }
+                }
+                else if (part == ".")
+                {
+                    continue;
+                }
+                else
+                {
+                    if (current is Directory directory)
+                    {
+                        IFileSystemItem? item = directory.Items.Find(x => x.Name == part);
+                        if (item == null)
+                        {
+                            throw new FileSystemException($"No such file or directory: {part}");
+                        }
+
+                        current = item;
+                    }
+                    else
+                    {
+                        throw new FileSystemException($"{current.Name} is not a directory.");
+                    }
+                }
+            }
+
+            return current;
+        }
+
+        public string GetPath()
+        {
+            if (Parent == null)
+            {
+                return "/";
+            }
+
+            string parentPath = Parent.GetPath();
+
+            if (parentPath == "/")
+            {
+                return $"/{Name}";
+            }
+
+            return $"{parentPath}/{Name}";
         }
 
         public bool IsDirectory(string name)
