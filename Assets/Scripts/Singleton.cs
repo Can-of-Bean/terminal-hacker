a5081e228c27ac0edfe84fc469538a3620d4fce0
Solar <bmacaulay4843@gmail.com>
2024-02-24 13:54:39 +0000

    Created a Singleton class for non-mono behaviour things such as File Systems

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class Singleton<T>
+    where T : Singleton<T>, new()
+{
+    public static T Instance { get; private set; } = null!;
+    
+    public Singleton()
+    {
+        if (Instance != null)
+            throw new InvalidOperationException("Cannot create duplicate instance of singleton behaviour");
+
+        Instance = new T();
+    }
+}
