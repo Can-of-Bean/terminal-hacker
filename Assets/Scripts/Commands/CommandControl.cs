a46eabf4c26c911b556f33984b97fcfa6d3ed0ad
Fae Burns <fae.burns0@gmail.com>
2024-02-27 16:35:53 +0000

    Applied code style and implemented command to exit from ssh session

--- Assets/Scripts/Commands/CommandControl.cs
+++ Assets/Scripts/Commands/CommandControl.cs
@@ -7,20 +7,21 @@ namespace Commands
 {
     public class CommandControl : SingletonBehaviour<CommandControl>
     {
-        private Dictionary<string, ICommand> commands;
+        private Dictionary<string, ICommand> m_commands = null!;
 
         public void Start()
         {
             TerminalControl.Instance.RawInputSubmitted += OnRawInputSubmitted;
 
             // Initialize the commands dictionary
-            commands = new Dictionary<string, ICommand>
+            m_commands = new Dictionary<string, ICommand>
             {
                 { "ls", new ListCommand() },
                 { "cat", new CatCommand() },
                 { "ssh", new SshCommand()},
                 { "cd", new ChangeDirectoryCommand() },
-                { "mkdir", new MakeDirectoryCommand() }
+                { "mkdir", new MakeDirectoryCommand() },
+                { "exit", new ExitSshCommand() },
                 // { "touch", new TouchCommand() },
                 // { "rm", new RemoveCommand() },
                 // { "clear", new ClearCommand() },
@@ -35,7 +36,7 @@ namespace Commands
             string cmd = e.Input.Split(' ')[0];
             string[] args = e.Input.Split(' ')[1..] ?? Array.Empty<string>();
 
-            if (commands.TryGetValue(cmd, out ICommand command))
+            if (m_commands.TryGetValue(cmd, out ICommand command))
             {
                 try
                 {
--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -4,18 +4,7 @@ using UnityEngine;
 public class Singleton<T>
     where T : Singleton<T>, new()
 {
-    private static T? _instance;
+    private static T? s_instance;
 
-    public static T Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                _instance = new T();
-            }
-
-            return _instance;
-        }
-    }
+    public static T Instance => s_instance ??= new T();
 }
